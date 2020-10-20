0b79e754ef65029a8b90cdc5320f6fd8974f64da
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-10-20 14:36:53 -0400

    8-passed

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 // Your code below
 
+function isItColin(str){
+  return (str === 'Colin' ? 'Yes, that is Colin.' : 'No, that is not Colin.')
+}
 
+function isHot(temperature){
+  return (temperature >= 80 ? 'Yes, it is indeed hot.' : 'No, it is not hot.')
+}
 
+function helloThere(name){
+  return (name.length >= 6 ? 'Hi' + ', ' + name : 'Hello' + ', ' + name)
+}
+
+function goodbyeYou(name){
+  return (name = ' ' ? 'Goodbye' + ', ' + name : 'Goodbye' + ', ' + 'stranger')
+}
 
 
 // Our code below; do not touch!
